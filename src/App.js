8d7bfd9d836b15fd63c7274fb5eeffda7db247a4
AkshithGandla <akshithgandla7891@gmail.com>
2023-06-01 15:33:57 +0530

    complete navbar and searchbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Navbar,
+  Feed,
+  SearchFeed,
+  VideoDetail,
+  ChannelDetail,
+} from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ background: "#000" }}>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" exact element={<Feed></Feed>}></Route>
+          <Route
+            path="/video/:id"
+            element={<VideoDetail></VideoDetail>}
+          ></Route>
+          <Route
+            path="/channel/:id"
+            element={<ChannelDetail></ChannelDetail>}
+          ></Route>
+          <Route
+            path="/search/:searchTerm"
+            element={<SearchFeed></SearchFeed>}
+          ></Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
